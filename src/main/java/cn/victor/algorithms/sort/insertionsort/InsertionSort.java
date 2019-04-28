c445ca39efc958bca0fdd31aa414e4fa9bd1e497
victor.chu <victorchu0610@outlook.com>
2019-04-28 15:09:11 +0800

    提交插入排序的实现

---
+++ src/main/java/cn/victor/algorithms/sort/insertionsort/InsertionSort.java
@@ -0,0 +1,54 @@
+package cn.victor.algorithms.sort.insertionsort;
+
+import cn.victor.algorithms.sort.Sorter;
+import cn.victor.algorithms.sort.selectionsort.SelectionSort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @Author victor
+ * @Email victorchu0610@outlook.com
+ * @Data 2019/4/28
+ * @Version 1.0
+ * @Description 插入排序的实现
+ */
+public class InsertionSort<T> extends Sorter<T> {
+    public InsertionSort(Comparator<T> comparator) {
+        super(comparator);
+    }
+
+    @Override
+    protected void sort(T[] array) {
+        for (int i = 1; i < array.length; i++) {
+            T target = array[i];
+            int j = i - 1;
+
+            // 将比目标大的元素都向后移动一位
+            while (j >= 0 && comparator.compare(array[j],target)>0) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = target;
+        }
+    }
+
+    public static void main(String[] args) {
+        InsertionSort<Integer> InsertionSort = new InsertionSort<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (o1 > o2) {
+                    return 1;
+                }
+                if (o1.equals(o2)) {
+                    return 0;
+                }
+                return -1;
+            }
+        });
+
+        Integer[] integers = new Integer[]{32, 4254, 124, 54, 24, 6763, 3435, 12344, 63, 254};
+        InsertionSort.sort(integers);
+        System.out.println(Arrays.toString(integers));
+    }
+}
