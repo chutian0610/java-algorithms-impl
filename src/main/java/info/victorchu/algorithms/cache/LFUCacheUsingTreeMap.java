569e998f2acb4f782e8bacb60431758bce917880
chutian <victorchu0610@outlook.com>
2022-11-18 20:00:06 +0800

    M: LRU

---
+++ src/main/java/info/victorchu/algorithms/cache/LFUCacheUsingTreeMap.java
@@ -0,0 +1,11 @@
+package info.victorchu.algorithms.cache;
+
+/**
+ * @Copyright:www.xiaojukeji.com Inc. All rights reserved.
+ * @Description:
+ * @Date:2022/11/18 17:00
+ * @Author:victorchutian
+ */
+public class LFUCacheUsingTreeMap<K, V> {
+
+}
